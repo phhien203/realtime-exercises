4df9d75dcf22a240ba1891ba2de65c2de6b7adb7
Pham Huu Hien <phhien203@gmail.com>
2023-01-01 19:27:26 +0700

    do polling using setTimeout

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -15,13 +15,39 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  // post to /poll a new message
-  // write code here
+  const data = {
+    user,
+    text,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = await fetch("/poll", options);
+  const json = await res.json();
 }
 
 async function getNewMsgs() {
-  // poll the server
-  // write code here
+  let json;
+
+  try {
+    const res = await fetch("/poll");
+    json = await res.json();
+  } catch (e) {
+    // backoff code
+    console.error("polling error");
+  }
+
+  allChat = json.msg;
+  render();
+  setTimeout(() => {
+    getNewMsgs();
+  }, INTERVAL);
 }
 
 function render() {
